5dcc94ea7fc2ef7b43625f0ba181f3bea0636949
CodeArtisT75 <pcstar7000@gmail.com>
2020-07-27 13:40:51 +0430

    add bootstrapVue plugin to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/**
+ * plugins
+ */
+import './plugins/VueBootstrap';
+
 Vue.config.productionTip = false
 
 new Vue({
